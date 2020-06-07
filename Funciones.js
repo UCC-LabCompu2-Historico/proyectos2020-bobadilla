38cb87ab8fd1bf774e5dbf5dda34f322cd444198
Manuel-Bobadilla <manuelcba@outlook.com>
2020-06-07 21:50:13 +0200

    js para ecuaciones

--- Funciones.js
+++ Funciones.js
@@ -6,16 +6,17 @@ function CalculoIndex(){
 	var seno =Math.sin(angulorad);
 	var coseno =Math.cos(angulorad);
 	var tangente =Math.tan(angulorad);
+	var seno2=seno;//copia del valor de seno sin redondear, se utilizara para determinar cuando es posible calcular la tangente
 	coseno=Math.round(coseno*1000)/1000;
 	seno=Math.round(seno*1000)/1000;
 	tangente=Math.round(tangente*1000)/1000;
 	document.getElementById("rsen").value=seno;
 	document.getElementById("rcos").value=coseno;
-	if(seno==1){
+	if(seno2==1){
 		var tangente2="Ind";
 		document.getElementById("rtan").value=tangente2;
 	}
-	if(seno!=1){
+	if(seno2!=1){
 		document.getElementById("rtan").value=tangente;
 	}
 	
@@ -48,16 +49,18 @@ function CalculoIndex(){
 	ctx.stroke();
 	ctx.closePath();
 	
-	ctx.beginPath();//tangente
-	ctx.moveTo(anchoMax/2,alturaMax/2);
-	ctx.lineTo(anchoMax/2+150,alturaMax/2-tangente*150);
-	ctx.lineTo(anchoMax/2+150,alturaMax/2);
-	ctx.strokeStyle="#f00";
-	ctx.stroke();
-	ctx.stroke();
-	ctx.closePath();
+	if(document.getElementById("tan").checked){
+		ctx.beginPath();//tangente
+		ctx.moveTo(anchoMax/2,alturaMax/2);
+		ctx.lineTo(anchoMax/2+150,alturaMax/2-tangente*150);
+		ctx.lineTo(anchoMax/2+150,alturaMax/2);
+		ctx.strokeStyle="blue";
+		ctx.stroke();
+		ctx.stroke();
+		ctx.closePath();
+	}
 	
-	if(document.getElementById("sen").value){
+	if(document.getElementById("sen").checked){
 		ctx.beginPath();//seno
 		ctx.moveTo(anchoMax/2+coseno*150,alturaMax/2);
 		ctx.lineTo(anchoMax/2+coseno*150,alturaMax/2-seno*150);
@@ -66,16 +69,17 @@ function CalculoIndex(){
 		ctx.stroke();
 		ctx.closePath();
 	}
-	ctx.beginPath();//cose
-	ctx.moveTo(anchoMax/2,alturaMax/2);
-	ctx.lineTo(anchoMax/2+coseno*150,anchoMax/2);
-	ctx.strokeStyle="#150";
-	ctx.stroke();
-	ctx.stroke();
-	ctx.closePath();
 	
-	
-	
+	if(document.getElementById("cos").checked){
+		ctx.beginPath();//cose
+		ctx.moveTo(anchoMax/2,alturaMax/2);
+		ctx.lineTo(anchoMax/2+coseno*150,anchoMax/2);
+		ctx.strokeStyle="red";
+		ctx.stroke();
+		ctx.stroke();
+		ctx.closePath();
+	}
+
 	ctx.beginPath();//angulo
 	ctx.moveTo(anchoMax/2,alturaMax/2);
 	ctx.lineTo(anchoMax/2+coseno*150,alturaMax/2-seno*150);
@@ -112,5 +116,54 @@ function Cuadricula(){
 	ctx.stroke();
 	ctx.stroke();
 	ctx.closePath();
+}
+
+function calcseno(){
+	var cosen;
+	var hipsen;
+	cosen=Number(document.getElementById("cosen").value);
+	hipsen=Number(document.getElementById("hipsen").value);
+	if(cosen>hipsen){//es imposible que el cateto sea mas grande que la hipotenusa
+		var imp="Imp"
+		document.getElementById("ressen").value=imp;//no se imprime nada en pantalla
+	}
+	else{
+		document.getElementById("ressen").value=cosen/hipsen;
+	}
 	
 }
+
+function calccoseno(){
+	var cacos;
+	var hipcos;
+	cacos=Number(document.getElementById("cacos").value);
+	hipcos=Number(document.getElementById("hipcos").value);
+	if(cacos>hipcos){//es imposible que el cateto sea mas grande que la hipotenusa
+		var imp="Imp"
+		document.getElementById("rescos").value=imp;//no se imprime nada en pantalla
+	}
+	else{
+		document.getElementById("rescos").value=cacos/hipcos;
+	}
+	
+}
+
+function calctangente(){
+	var cotan;
+	var catan;
+	cotan=Number(document.getElementById("cotan").value);
+	catan=Number(document.getElementById("catan").value);
+	if(catan==0){//es imposible la division por 0
+		var imp="Imp"
+		document.getElementById("restan").value=imp;//no se imprime nada en pantalla
+	}
+	else{
+		document.getElementById("restan").value=cotan/catan;
+	}
+	
+}
+
+
+
+
+
